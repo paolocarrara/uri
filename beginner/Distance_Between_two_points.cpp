30b428326e00a9f1a05d14cf19bade9e74685312
Paolo Carrara <paolocarraratux@gmail.com>
2014-08-10 15:14:56 -0300

    Distance Between two points

---
+++ beginner/Distance_Between_two_points.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void)
+{
+	int x1, y1, x2, y2;
+	scanf ("%i %i %i %i", &x1, &y1, &x2, &y2);
+	printf ("%0.4f\n", sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))));
+	return 0;
+}
