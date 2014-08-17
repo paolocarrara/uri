6bed9a7484d069559ea7a61c824bb2b4e7d82726
Paolo Carrara <paolocarraratux@gmail.com>
2014-08-17 16:35:56 -0300

    Above the Main Diagonal

---
+++ beginner/Above_the_Main_Diagonal.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int line, i, j;
+	char op;
+	float m[12][12];
+	float r;
+
+	scanf ("%c", &op);
+
+	for (i = 0; i < 12; i++) {
+		for (j = 0; j < 12; j++) {
+			scanf ("%f", &m[i][j]);
+		}
+	}
+
+	r = 0;
+	for (i = 0; i < 11; i++) {
+		for (j = 1+i; j < 12; j++) {
+			r += m[i][j];
+		}
+	}
+
+
+
+	if (op == 'S') {
+		printf ("%0.1f\n", r);
+	}
+	else {
+		printf ("%0.1f\n", r/(66*(1.0)));
+	}
+
+	return 0;
+}
