5e5fde60b3117ca1f29e64dbf4f826cf6fe304d0
Paolo Carrara <paolocarraratux@gmail.com>
2014-08-17 23:40:39 -0300

    asdf

---
+++ beginner/Superior_Area.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int i, j;
+	char op;
+	float m[12][12];
+	float r;
+
+	scanf ("%c", &op);
+
+	for (i = 0; i < 12; i++) {
+		for (j = 0; j < 12; j++) {
+			scanf ("%f", &m[i][j]);
+		}
+	}
+
+	r = 0;
+	for (i = 0; i < 5; i++) {
+		for (j = 1 + i; j < 11 - i; j++) {
+			r += m[i][j];
+		}
+	}
+
+	if (op == 'S') {
+		printf ("%0.1f\n", r);
+	}
+	else {
+		printf ("%0.1f\n", r/(30*(1.0)));
+	}
+
+	return 0;
+}
