9da2bc9177a5f8b984943c97d72c505aa7eaed1a
Paolo Carrara <paolocarraratux@gmail.com>
2014-08-18 22:27:33 -0300

    Array Selection I

--- beginner/Array_Selection_I.cpp
+++ beginner/Array_Selection_I.cpp
@@ -4,11 +4,19 @@ int main (void) /*rever*/
 {
 	float n;
 	int i;
+	int intpart;
+	float floatpart;
 
 	for (i = 0; i < 100; i++) {
 		scanf ("%f", &n);
-		if (n <= 10.0)
-			printf ("A[%i] = %0.1f\n", i, n);
+		if (n <= 10.0) {
+			intpart = (int)n;
+			floatpart = n - intpart;
+			if (floatpart == 0)
+				printf ("A[%i] = %i\n", i, intpart);
+			else
+				printf ("A[%i] = %0.1f\n", i, n);
+		}
 	}
 
 	return 0;
