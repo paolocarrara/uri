f57c47cd111cd4154b6c899f64693e1aa2ad417e
Paolo Carrara <paolocarraratux@gmail.com>
2014-08-19 18:04:06 -0300

    Brazil World Cup

--- beginner/Brazil_World_Cup.cpp
+++ beginner/Brazil_World_Cup.cpp
@@ -5,10 +5,11 @@ int main (void)
 	short int n = 1;
 	while (n != EOF) {
 		scanf ("%hi", &n);
-		if (n > 0)
-			puts ("vai ter duas!");
-		else
-			puts ("vai ter copa!");
+		if (n != EOF)
+			if (n > 0)
+				puts ("vai ter duas!");
+			else
+				puts ("vai ter copa!");
 	}
 
 	return 0;
